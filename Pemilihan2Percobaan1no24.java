1cc9d7a7ec199b1737d8f3f9927408a44cf6d2a1
Raihanbujeee <bujexixi@gmail.com>
2024-10-02 16:42:51 +0700

    mod1

--- Pemilihan2Percobaan1no24.java
+++ Pemilihan2Percobaan1no24.java
@@ -1,3 +1,24 @@
+// import java.util.Scanner;
+// public class Pemilihan2Percobaan1no24 {
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int tahun;
+
+//         System.out.println("masukkan tahun:");
+//         tahun = input.nextInt();
+//         if ((tahun % 4 ) == 0) {
+//             if ((tahun % 100) !=0)
+//             System.out.println("tahun kabisat"); 
+                
+//         } else
+//         System.out.println("bukan tahun kabisat");
+//          {
+            
+//         } 
+            
+//         }
+//     }
 import java.util.Scanner;
 public class Pemilihan2Percobaan1no24 {
 
@@ -5,17 +26,21 @@ public class Pemilihan2Percobaan1no24 {
         Scanner input = new Scanner(System.in);
         int tahun;
 
-        System.out.println("masukkan tahun:");
+        System.out.print("masukkan tahun:");
         tahun = input.nextInt();
-        if ((tahun % 4 ) == 0) {
-            if ((tahun % 100) !=0)
-            System.out.println("tahun kabisat"); 
-                
-        } else
-        System.out.println("bukan tahun kabisat");
-         {
-            
-        } 
-            
+
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("tahun kabisat");
+                } else {
+                    System.out.println("bukan tahun kabisat");
+                }
+            } else {
+                System.out.println("tahun kabisat");
+            }
+        } else {
+            System.out.println("bukan tahun kabisat");
         }
     }
+}
