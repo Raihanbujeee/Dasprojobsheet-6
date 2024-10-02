01cbcae2c7b86256cac4878b67d8e5a62fd01384
Raihanbujeee <bujexixi@gmail.com>
2024-10-02 16:35:53 +0700

    1

---
+++ Pemilihan2Percobaan1no24.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan1no24 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int tahun;
+
+        System.out.println("masukkan tahun:");
+        tahun = input.nextInt();
+        if ((tahun % 4 ) == 0) {
+            if ((tahun % 100) !=0)
+            System.out.println("tahun kabisat"); 
+                
+        } else
+        System.out.println("bukan tahun kabisat");
+         {
+            
+        } 
+            
+        }
+    }
