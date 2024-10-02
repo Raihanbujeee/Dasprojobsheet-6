7a473d5b8d76e3703fa94823673dd1ed5b239c79
Raihanbujeee <bujexixi@gmail.com>
2024-10-02 16:51:08 +0700

    mod2

--- Pemilihan2Percobaan1no24.java
+++ Pemilihan2Percobaan1no24.java
@@ -19,28 +19,56 @@
             
 //         }
 //     }
+
+// import java.util.Scanner;
+// public class Pemilihan2Percobaan1no24 {
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int tahun;
+
+//         System.out.print("masukkan tahun:");
+//         tahun = input.nextInt();
+
+//         if (tahun % 4 == 0) {
+//             if (tahun % 100 == 0) {
+//                 if (tahun % 400 == 0) {
+//                     System.out.println("tahun kabisat");
+//                 } else {
+//                     System.out.println("bukan tahun kabisat");
+//                 }
+//             } else {
+//                 System.out.println("tahun kabisat");
+//             }
+//         } else {
+//             System.out.println("bukan tahun kabisat");
+//         }
+//     }
+// }
+
 import java.util.Scanner;
+
 public class Pemilihan2Percobaan1no24 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int tahun;
 
-        System.out.print("masukkan tahun:");
+        System.out.println("Masukkan tahun:");
         tahun = input.nextInt();
 
-        if (tahun % 4 == 0) {
-            if (tahun % 100 == 0) {
-                if (tahun % 400 == 0) {
-                    System.out.println("tahun kabisat");
-                } else {
-                    System.out.println("bukan tahun kabisat");
+        if (tahun % 4 == 0) {            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) { 
+                    System.out.println("Tahun kabisat");
+                } else { 
+                    System.out.println("Bukan tahun kabisat");
                 }
-            } else {
-                System.out.println("tahun kabisat");
+            } else { 
+                System.out.println("Tahun kabisat");
             }
-        } else {
-            System.out.println("bukan tahun kabisat");
+        } else { 
+            System.out.println("Bukan tahun kabisat");
         }
     }
 }
+
